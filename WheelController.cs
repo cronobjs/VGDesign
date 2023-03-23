30a87b0ccbfe859e71fb4451ad29ed0fdf8f52ac
cronobjs <77480366+cronobjs@users.noreply.github.com>
2023-03-23 10:48:16 -0400

    Add files via upload

---
+++ WheelController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelController : MonoBehaviour
+{
+    public Rigidbody carRigidbody;
+    public Transform[] tireTransforms; //wheel transforms in the car, in the following order: FrontLeft, FrontRight, BackLeft, BackRight.
+    public float maxRotationSpeed = 100f;
+    public float rotationLerpSpeed = 10f;
+    public float steeringAngle = 30f;
+
+    private float currentRotationSpeed = 0f;
+    private float currentSteeringAngle = 0f;
+
+    private void FixedUpdate()
+{
+    float speed = carRigidbody.velocity.magnitude;
+    currentRotationSpeed = Mathf.Lerp(currentRotationSpeed, speed * maxRotationSpeed, rotationLerpSpeed * Time.deltaTime);
+
+    foreach (Transform tire in tireTransforms)
+    {
+        // Rotate around the Z-axis
+        tire.Rotate(Vector3.forward, currentRotationSpeed * Time.deltaTime);
+    }
+
+    float horizontalInput = Input.GetAxis("Horizontal");
+    currentSteeringAngle = Mathf.Lerp(currentSteeringAngle, horizontalInput * steeringAngle, rotationLerpSpeed * Time.deltaTime);
+
+    // Rotate around the Y-axis (left/right) and Z-axis (front/back)
+    tireTransforms[0].localRotation = Quaternion.Euler(0f, currentSteeringAngle, -currentRotationSpeed);
+    tireTransforms[1].localRotation = Quaternion.Euler(0f, currentSteeringAngle, -currentRotationSpeed);
+}
+
+}
---
+++ WheelPlacer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelPlacer : MonoBehaviour
+{
+    public GameObject[] wheels;
+    public RaycastSuspension raycastSuspension;
+    public float yOffset = 0.05f;
+
+    void Update()
+    {
+        // Get the hit points of the corner points from the RaycastSuspension script
+        Vector3[] hitPoints = raycastSuspension.GetCornerHitPoints();
+
+        // Update the position of each wheel
+        for (int i = 0; i < wheels.Length; i++)
+        {
+            Vector3 wheelPosition = hitPoints[i] + (Vector3.up * yOffset);
+            wheels[i].transform.position = wheelPosition;
+        }
+    }
+}
